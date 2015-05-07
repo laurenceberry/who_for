83b04074ad5c3c6091a117c7baacc065438eef85
Laurence Berry <berry.laur@gmail.com>
2015-05-07 09:46:14 +0100

    giving up now

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module WhoFor
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile += %w(.svg .eot .woff .ttf)
     config.serve_static_assets = true
+    config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
