191f20285592209c060d696fc8a25a2360ef8704
Laurence Berry <berry.laur@gmail.com>
2015-08-06 12:45:32 +0100

    removes validation for scalers

--- app/models/profile_scaler.rb
+++ app/models/profile_scaler.rb
@@ -1,6 +1,4 @@
 class ProfileScaler < ActiveRecord::Base
   belongs_to :profile
 
-  validates :scale, presence: true, numericality: true
-  validates :out_of, presence: true, numericality: true
 end
