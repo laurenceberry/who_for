4d36a8b2c46967066ffe6af687796c16c67d23b1
Laurence Berry <berry.laur@gmail.com>
2015-08-10 20:25:09 +0100

    removes unneeded code

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,10 +3,5 @@ class Ability
 
   def initialize(user)
 
-    can :read, :all
-    can :read, :all
-    can :create, :all
-    can :destroy, Ad do |ad|
-    profile.updated_by.to_s == user.id.to_s
-
+  end
 end
