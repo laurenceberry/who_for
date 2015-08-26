33ab6a1e56cc95cf9ba158cb3230eb58ea6da76c
Laurence Berry <berry.laur@gmail.com>
2015-08-26 12:16:37 +0100

    fix for logged out users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,11 @@ class PagesController < ApplicationController
   def index
     @user = current_user
     @packs = Pack.where('is_public = false')
-    @your_packs = Pack.where("created_by = '#{@user.id}'")
+    if current_user
+      @your_packs = Pack.where("created_by = '#{@user.id}'")
+    else
+      @your_packs = nil
+    end
   end
 
 end
